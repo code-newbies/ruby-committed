5a601068a78d7281378c13eb9a92f2dfd07c1753
Calvyn82 <claytonflesher@gmail.com>
2015-10-04 15:26:32 -0500

    pointed spec tests at development database and got first test passing

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:email) { |n| "user#{n}@example.com" }
-    password "password"    
-    password_confirmation "password"
-    verified_at { Time.now }
+    
   end
+
 end
