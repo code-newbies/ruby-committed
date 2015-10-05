602ca082dec51339f2c85240f72bc7f9e6266005
Calvyn82 <claytonflesher@gmail.com>
2015-10-04 20:17:43 -0500

    provided test coverage for sessions controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  it "prevents invalid users from signing in" do
+    user = FactoryGirl.create(:user)
+    post :create, email: user.email, password: ""
+    assert_response(:success)
+    expect(session[:user_id]).not_to eq(user.id)
+  end
 
+  it "signs in valid users" do
+    user = FactoryGirl.create(:user)
+    post :create, email: user.email, password: user.password
+    assert_response(:redirect)
+    expect(session[:user_id]).to eq(user.id)
+  end
 end
