23d384756dfa106db25c413a0ee99c0e883ca222
whicheverpeady <whicheverpeady@gmail.com>
2015-10-12 09:24:41 -0400

    Add context for invalid achievement specs

--- spec/models/achievement_spec.rb
+++ spec/models/achievement_spec.rb
@@ -10,28 +10,30 @@ RSpec.describe Achievement, type: :model do
       expect(achievement).to be_valid
     end
 
-    it "is invalid without a title" do
-      achievement.title = nil 
-      expect(achievement).to be_invalid
-    end
+    context "is invalid" do
+      it "without a title" do
+        achievement.title = nil 
+        expect(achievement).to be_invalid
+      end
 
-    it "is invalid without a description" do
-      achievement.description = nil 
-      expect(achievement).to be_invalid
-    end
+      it "without a description" do
+        achievement.description = nil 
+        expect(achievement).to be_invalid
+      end
 
-    it "is invalid if title already exists" do
-      achievement.save
-      duplicate_achievement = build(:achievement)
-      duplicate_achievement.title = achievement.title
-      expect(duplicate_achievement).to be_invalid
-    end
+      it "if title already exists" do
+        achievement.save
+        duplicate_achievement = build(:achievement)
+        duplicate_achievement.title = achievement.title
+        expect(duplicate_achievement).to be_invalid
+      end
 
-    it "is invalid if description already exists" do
-      achievement.save
-      duplicate_achievement = build(:achievement)
-      duplicate_achievement.description = achievement.description
-      expect(duplicate_achievement).to be_invalid
+      it "if description already exists" do
+        achievement.save
+        duplicate_achievement = build(:achievement)
+        duplicate_achievement.description = achievement.description
+        expect(duplicate_achievement).to be_invalid
+      end
     end
   end
 end
