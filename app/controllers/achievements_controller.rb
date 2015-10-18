83fb5fdcad4439746c79eacc7bbcdc582531e62e
whicheverpeady <whicheverpeady@gmail.com>
2015-10-18 11:29:10 -0400

    Add achievements controller

---
+++ app/controllers/achievements_controller.rb
@@ -0,0 +1,49 @@
+class AchievementsController < ApplicationController
+  before_action :find_achievement, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @achievement = Achievement.new
+  end
+
+  def create
+    @achievement = Achievement.new(achievement_params)
+    if @achievement.save
+      redirect_to @achievement
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def index
+    @achievements = Achievement.all
+  end
+
+  def edit
+  end
+
+  def update
+    if @achievement.update_attributes(achievement_params)
+      redirect_to @achievement
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @achievement.destroy
+    redirect_to root_path
+  end
+
+  private
+
+    def find_achievement
+      @achievement = Achievement.find(params[:id])
+    end
+
+    def achievement_params
+      params.require(:achievement).permit(:title, :description, :point)
+    end
+end
